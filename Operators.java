0dc86071ae1fef04b69b2fc575f4da35a1a18032
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-08-01 12:38:23 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -39,4 +39,23 @@ class Relational
      System.out.println("Is a not equal to b?"+(a!=b));
      }
      }
+//Logical Operators
+import java.util.*;
+class Logical
+{
+  public static void main(String[]args)
+  {
+     int a;
+     int b;
+     int c;
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Enter a, b and c values");
+     a=sc.nextInt();
+     b=sc.nextInt(); 
+     c=sc.nextInt();
+     System.out.println((a<b)&&(a<c));
+     System.out.println((a<b)||(a>c));
+     System.out.println(!(a>b));
+     }
+     }
 
