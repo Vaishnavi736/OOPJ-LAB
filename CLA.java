9a2899f9c98c8aa010ee12bc77c275d168a5d97f
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-08-08 11:37:52 +0530

    Create CLA.java

---
+++ CLA.java
@@ -0,0 +1,34 @@
+//
+import java.util.*;
+class CLA
+{
+   public static void main(String[]args)
+   {
+        System.out.println("The first argument is "+args[0]);
+        System.out.println("The second argument is "+args[1]);
+   }
+}        
+//
+import java.util.*;
+class CLA
+{
+   public static void main(String[]args)
+   {
+     for(int i=0;i<args.length;i++)
+     {
+     System.out.println(args[i]);
+     }  
+   }}
+      //
+import java.util.*;
+class Foreach
+{
+   public static void main(String[]args)
+   {
+        int arr[]={10,11,12,13};
+        for (int i:arr)
+        {
+           System.out.println(i);
+         }
+    }
+}           
