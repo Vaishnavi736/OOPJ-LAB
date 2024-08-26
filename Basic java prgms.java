0f7634ceb65ded7a782b215af24eeff2be045193
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-08-26 10:06:02 +0530

    Update Basic java prgms.java

--- Basic java prgms.java
+++ Basic java prgms.java
@@ -221,6 +221,32 @@ class Number
       }
     }
 }
+//LARGEST OF THREE NUMBERS
+import java.util.*;
+class Max
+{
+    public static void main(String[]args)
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter three numbers");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();  
+    if(a>=b&&a>=c)
+    {
+        System.out.println(a + " is largest");
+    }
+    else if(b>=a&&b>=c)
+    {
+       System.out.println(b + " is largest");
+    }
+    else
+    {
+          System.out.println(c + " is largest");
+    }
+}
+}
+
 
     
 
