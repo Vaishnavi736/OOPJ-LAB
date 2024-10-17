85c8f020515233da35b30000f4611135a3310520
Vaishnavi736 <161019478+Vaishnavi736@users.noreply.github.com>
2024-10-17 11:32:36 +0530

    Update exercise6.java

--- exercise6.java
+++ exercise6.java
@@ -53,5 +53,80 @@ class Multiple
                  System.out.println(npe);
             }
        }
- }                       
-                
+ }      
+//BUILT-IN EXCEPTIONS
+import java.util.*;
+class Built
+{
+      public static void main(String[]args)
+      {
+            try
+            {
+                 int arr[]=new int[5];
+                 System.out.println(arr[10]);
+            }
+            catch(ArrayIndexOutOfBoundsException aiobe)
+            {
+                System.out.println(aiobe);  
+            }
+            try
+            {
+                  int a=56;
+                  System.out.println(a/0);
+            }
+            catch(ArithmeticException ae)
+            {
+                System.out.println(ae);  
+            }    
+            try
+            {
+                 String str=null;
+                 System.out.println(str.length());
+            }      
+            catch(NullPointerException npe)
+            {
+                  System.out.println(npe);
+            }
+            finally
+            {
+                  System.out.println("These are some of the built-in exceptions");
+            }
+       }                   
+}       
+//OUTPUT
+java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 5
+java.lang.ArithmeticException: / by zero
+java.lang.NullPointerException: Cannot invoke "String.length()" because "<local1>" is null
+These are some of the built-in exceptions
+ //USER-DEFINED EXCEPTION
+    import java.util.*;
+public class Checks
+{
+      public static void main(String[]args)
+      {
+             Scanner sc=new Scanner(System.in);
+             System.out.println("Enter age");
+             int a=sc.nextInt();
+             User.validate(a);
+      }    
+}
+class User 
+{
+       public static void validate(int age)
+       {
+            if(age<18)
+            {
+                 throw new ArithmeticException("Not eligible to vote");
+            }
+            else
+            {
+                     System.out.println("Eligible to vote");
+            }
+        }
+ }                          
+//OUTPUT
+Enter age
+1    
+Exception in thread "main" java.lang.ArithmeticException: Not eligible to vote
+	at User.validate(Checks.java:18)
+	at Checks.main(Checks.java:9)
